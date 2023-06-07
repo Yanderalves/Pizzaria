f3f842197e3d39a48e3dd6a188c06f8cff7e552c
Yander Alves de Carvalho <yander.carvalho@fitbank.com.br>
2023-06-07 09:47:53 -0300

    Tratamento de erros na api

--- pizzaria/backend/src/server.ts
+++ pizzaria/backend/src/server.ts
@@ -1,10 +1,27 @@
-import { router } from './routes';
 import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors"
+import cors from "cors";
+
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
-
 app.use(router);
+app.use(cors());
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) =>{
+    if(err instanceof Error){
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: "Request foi de vasco HAHAHAHAHHAHAHAHAHAH"
+
+    })
+})
 
 app.listen(3030);
